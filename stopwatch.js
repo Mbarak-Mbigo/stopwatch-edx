b3be79374f2a132d7097a4deb40e437ea0312bca
Mbarak Mbigo <mbarak.mbigo@andela.com>
2017-11-15 17:35:18 +0300

    feat(stopwatch): add functionality
    - add interface
    - add styling
    - add js logic
    - refactor

--- stopwatch.js
+++ stopwatch.js
@@ -9,4 +9,104 @@
  * Reset the timer count to 0 and wipe all previously recorded times in the Past Times section by pressing the 'r' key.
  */
 
-console.log('all works fine');
+// Global variables
+var timerStarted = false;
+var time = 0;
+var timeSplits = [];
+var interval;
+var elapsedTime = document.getElementById('elapsedTime');
+
+// Run code
+setUp();
+
+// Function definitions
+/**
+ * Initialize application
+ */
+function setUp() {
+    elapsedTime.innerHTML = 0;
+}
+
+/**
+ * start/stop timer
+ */
+function startStopTimer() {
+    if (timerStarted) {
+        clearInterval(interval);
+        timerStarted = false;
+    } else {
+        timerStarted = true;
+        interval = setInterval(function () {
+            time++;
+            elapsedTime.innerHTML = (time/1000).toFixed(2);
+        }, 1)
+    }
+}
+
+/**
+ * reset stopwatch
+ */
+function reset() {
+    timerStarted = false;
+    elapsedTime.innerHTML = time = 0;
+    clearInterval(interval);
+    var recordTimeSplits = document.getElementById('pastTimes');
+    removeallChildNodes(recordTimeSplits);
+}
+
+/**
+ * record timeSplits
+ */
+function recordTime() {
+    timeSplits.push(time/1000);
+    displayTimeSplits();
+}
+
+// helper functions
+function displayTimeSplits() {
+    var recordTimeSplits = document.getElementById('pastTimes');
+    var lastChild = recordTimeSplits.lastChild;
+
+    if(timeSplits.length) {
+        if(lastChild) {
+            if(timeSplits[timeSplits.length - 1].toFixed(2) !== lastChild.textContent) {
+                appendChildElement(recordTimeSplits);
+            }
+        } else {
+            appendChildElement(recordTimeSplits);
+        }
+    }
+}
+
+function appendChildElement(parent) {
+    var newElement = document.createElement('p');
+    var content = document.createTextNode(timeSplits[timeSplits.length-1].toFixed(2));
+    newElement.appendChild(content);
+    parent.appendChild( newElement);
+}
+
+function removeallChildNodes(parent) {
+    while(parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+document.getElementById('start-stop').addEventListener('click', startStopTimer);
+
+document.getElementById('reset').addEventListener('click', reset);
+
+document.getElementById('recordTime').addEventListener('click', recordTime);
+
+document.addEventListener('keydown', function (event) {
+    if (event.defaultPrevented) {
+        return; // Do nothing if the event was already processed
+    }
+
+    if(event.key === 's') {
+        startStopTimer();
+    } else if (event.key === 'r') {
+        reset();
+    } else if (event.key === 't') {
+        recordTime();
+    }
+});
