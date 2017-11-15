9ae19bf52465aaa9f6fcebc6c9a17747abc50ab9
Mbarak Mbigo <mbarak.mbigo@andela.com>
2017-11-15 17:30:36 +0300

    chore(setup): add project files

---
+++ stopwatch.js
@@ -0,0 +1,12 @@
+/**
+ * Created by mbarakmbigo on 14/11/2017.
+ *
+ * Start and stop the timer by pressing the "Start/Stop" button.
+ * Start and stop the timer by pressing the 's' key.
+ * Record the current timer count into the Past Times section by pressing the "Record Time" button
+ * Record the current timer count into the Past Times section by pressing the 't' key.
+ * Reset the timer count to 0 and wipe all previously recorded times in the Past Times section by pressing the "Reset" button.
+ * Reset the timer count to 0 and wipe all previously recorded times in the Past Times section by pressing the 'r' key.
+ */
+
+console.log('all works fine');
